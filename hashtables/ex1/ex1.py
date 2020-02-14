a3dcc6cc639b121801ac47a35f9589fd2dbe1a2a
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-14 11:55:50 -0500

    First pass of Ex1 passed tests.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -12,7 +10,21 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # hash_table_insert(hash_table, key, value)
+    # hash_table_retrieve(hash_table, key)
 
+    for curr in range(len(weights)):
+            
+        wt=weights[curr]
+        opp=limit-wt
+        htop=hash_table_retrieve(ht, opp)
+        if htop==None:
+            hash_table_insert(ht, wt, curr)
+        else:
+            if htop>curr:
+                return (htop,curr)
+            else:
+                return (curr,htop)
     return None
 
 
